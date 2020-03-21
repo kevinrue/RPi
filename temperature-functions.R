a1122919008b765f30708cf466c67c58087d38c7
Kevin Rue-Albrecht <kevinrue67@gmail.com>
2020-03-21 16:54:57 +0000

    helper functions

---
+++ temperature-functions.R
@@ -0,0 +1,79 @@
+library(tidyverse)
+
+# Functions ----
+
+#' Title
+#'
+#' @param file
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' temperature_input_table <- read_temperature_file("examples/temperature.txt")
+#' temperature_input_table <- read_temperature_file("examples/temperature_30min.txt")
+read_temperature_file <- function(file) {
+  read_delim(file, "\t")
+}
+
+#' Title
+#'
+#' @param data
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' temperature_input_table <- read_temperature_file("examples/temperature.txt")
+#'
+#' temperature_input_table %>% add_diff_temperature()
+add_diff_temperature <- function(data) {
+  data %>% mutate(diff = c(0, diff(temperature)))
+}
+
+#' Title
+#'
+#' @param data
+#' @param min_increase
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' temperature_input_table <- read_temperature_file("examples/temperature.txt")
+#'
+#' temperature_input_table %>% first_valid_timepoint()
+first_valid_timepoint <- function(data, min_increase = 0.5) {
+  # 'data' must contain columns 'temperature' and 'time'
+  data %>%
+    add_diff_temperature() %>%
+    mutate(increase = diff > min_increase) %>%
+    subset(increase) %>%
+    pull(time) %>% head(1)
+}
+
+#' Title
+#'
+#' @param data
+#' @param ...
+#' @param reset
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' temperature_input_table <- read_temperature_file("examples/temperature.txt")
+#'
+#' temperature_input_table %>% drop_pre_measurements()
+drop_pre_measurements <- function(data, ..., reset = TRUE) {
+  time_start <- data %>% first_valid_timepoint(...)
+
+  data <- data %>%
+    subset(time >= time_start)
+
+  if (reset) {
+    data <- data %>% mutate(time = time - min(time))
+  }
+
+  data
+}
